779280ba0e7b6c9c8c517e04268cd08d65c58f3f
Mosaab Abazid <mosaab@spotlightstudios.live>
2026-08-23 02:57:27 +0200

    add Hetzner auto deploy

--- Workspace/client/next.config.mjs
+++ Workspace/client/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  turbopack: {
+    root: new URL(".", import.meta.url).pathname,
+  },
   async headers() {
     return [
       {
